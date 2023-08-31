c491ac569046a9ed0215db3cfeedb0b755ffe787
parkchwl <93250361+parkchwl@users.noreply.github.com>
2023-08-31 12:33:22 +0900

    cpp

--- RacingCar.cpp
+++ RacingCar.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 #define ID_LEN 20
-# define MAX_SPD 200
+#define MAX_SPD 200
 #define FUEL_STEP 2
 #define ACC_STEP 10
 #define BRK_STEP 10
@@ -38,3 +38,28 @@ void Accel(Car &car)
 }
 
 void Break(Car &car)
+{
+  if(car.curSpeed<BRK_STEP)
+  {
+    car.curSpeed=0;
+    return;
+  }
+
+  car.curSpeed-=BRK_STEP;
+}
+
+int main()
+{
+  Car run99={"run99", 100, 0};
+  Accel(run99);
+  Accel(run99);
+  ShowCarState(run99);
+  Break(run99);
+  ShowCarState(run99);
+
+  Car sped77={"sped77", 100, 0};
+  Accel(sped77);
+  Break(sped77);
+  ShowCarState(sped77);
+  return 0;
+}
